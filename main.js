cd5b5cabb5202d444ba9cf094a0244d1f03de602
timhaller <timhaller@users.noreply.github.com>
2023-09-15 15:37:02 +0000

    Prettified Code!

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
-    randomString = length => Array.from(Array(length)).map(randomChar).join("");
+  randomString = (length) => Array.from(Array(length)).map(randomChar).join("");
 
 const letters = document.getElementById("letters");
 
 function MyFunction() {
-    letters.innerHTML = randomString(1500)
+  letters.innerHTML = randomString(1500);
 }
 
 letters.onmousemove = function (e) {
-    letters.style.setProperty("--x", e.clientX + "px");
-    letters.style.setProperty("--y", e.clientY + "px");
-    letters.innerText = randomString(1500)
-}
-
+  letters.style.setProperty("--x", e.clientX + "px");
+  letters.style.setProperty("--y", e.clientY + "px");
+  letters.innerText = randomString(1500);
+};
