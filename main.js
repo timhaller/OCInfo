e85d95bc6c659dee9de0a758195849d7080387f1
timhaller <67918170+timhaller@users.noreply.github.com>
2023-09-21 19:16:40 +0200

    Worked on some stuff

--- main.js
+++ main.js
@@ -3,15 +3,12 @@ const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
     randomString = length => Array.from(Array(length)).map(randomChar).join("");
 
-const letters = document.getElementById("letters");
+const letters = document.getElementById("card-letters");
 
-function MyFunction() {
-    letters.innerHTML = randomString(1500)
+function loop() {
+    document.getElementById("card-letters").innerHTML = randomString(3200)
 }
 
-letters.onmousemove = function (e) {
-    letters.style.setProperty("--x", e.clientX + "px");
-    letters.style.setProperty("--y", e.clientY + "px");
-    letters.innerText = randomString(1500)
+window.onload = () => {
+    setInterval(loop, 200);
 }
-
