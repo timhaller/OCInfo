77c5045f2dac1133172162ed374241d37bc9f5b2
timhaller <timhaller@users.noreply.github.com>
2023-09-21 17:18:43 +0000

    Prettified Code!

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))],
-    randomString = length => Array.from(Array(length)).map(randomChar).join("");
+  randomString = (length) => Array.from(Array(length)).map(randomChar).join("");
 
 const letters = document.getElementById("card-letters");
 
 function loop() {
-    document.getElementById("card-letters").innerHTML = randomString(3200)
+  document.getElementById("card-letters").innerHTML = randomString(3200);
 }
 
 window.onload = () => {
-    setInterval(loop, 200);
-}
+  setInterval(loop, 200);
+};
