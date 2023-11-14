34b4e28e5642d3c74023aa56774341871b9d68cf
timhaller <timhaller@users.noreply.github.com>
2023-11-14 12:35:53 +0000

    Prettified Code!

--- exercices/m05/ex1/main.js
+++ exercices/m05/ex1/main.js
@@ -1,16 +1,15 @@
-do{
-    var num1 = Number(prompt("Numéro 1: "));
-} while(isNaN(num1) || num1 == null);
+do {
+  var num1 = Number(prompt("Numéro 1: "));
+} while (isNaN(num1) || num1 == null);
 
-do{
-    var num2 = Number(prompt("Numéro 2: "));
-} while(isNaN(num2) || num2 == null || num2 < num1)
+do {
+  var num2 = Number(prompt("Numéro 2: "));
+} while (isNaN(num2) || num2 == null || num2 < num1);
 
 let numbers = [];
-for (let i = num1; i <= (num2 - num1); i++) {
-    numbers.push(i);
+for (let i = num1; i <= num2 - num1; i++) {
+  numbers.push(i);
 }
 numbers.push(num2);
-
 
 alert(numbers.join(", "));
