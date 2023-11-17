fa20db041760e0f8563baaebd1ac953b92759112
timhaller <timhaller@users.noreply.github.com>
2023-11-17 14:08:38 +0000

    Prettified Code!

--- exercices.js
+++ exercices.js
@@ -17,30 +17,35 @@ document.addEventListener("DOMContentLoaded", async function () {
     .then((response) => response.json())
     .then((data) => {
       return data.modules;
-    }
-  )
+    });
 
   function checkPath(list, path) {
-    return list.find(element => element.name === path) !== undefined;
+    return list.find((element) => element.name === path) !== undefined;
   }
 
   async function processCommand(command) {
     // Add your command processing logic here
-    switch (command[0]){
+    switch (command[0]) {
       case "hello":
         writeToTerminal("Hello World!");
         break;
       case "ls":
         let dirToList = command[1] || path;
         console.log(checkPath(files, dirToList));
-        let module = checkPath(files, dirToList)? files.find(element => element.name === dirToList).exercices.map(element => element.name) : files.map(element => element.name)
+        let module = checkPath(files, dirToList)
+          ? files
+              .find((element) => element.name === dirToList)
+              .exercices.map((element) => element.name)
+          : files.map((element) => element.name);
         console.log(module);
         writeToTerminal(module.join(" "));
         break;
       case "cd":
         let dirToGo = command[1] || "~";
         console.log(dirToGo);
-        checkPath(files, dirToGo) || dirToGo === "~"? (path = dirToGo, pathField.innerHTML = `${path}`) : writeToTerminal(`Directory not found: ${dirToGo}`);
+        checkPath(files, dirToGo) || dirToGo === "~"
+          ? ((path = dirToGo), (pathField.innerHTML = `${path}`))
+          : writeToTerminal(`Directory not found: ${dirToGo}`);
         break;
       case "clear":
         terminalOutput.innerHTML = "";
@@ -53,18 +58,20 @@ document.addEventListener("DOMContentLoaded", async function () {
         }
         let basePath = path;
         if (fileToOpen.split("/").length > 1) {
-            basePath = fileToOpen.split("/")[0];
-            fileToOpen = fileToOpen.split("/")[1];
+          basePath = fileToOpen.split("/")[0];
+          fileToOpen = fileToOpen.split("/")[1];
         }
         let exercice = {};
         if (checkPath(files, basePath)) {
-          exercice = files.find(element => element.name === basePath).exercices.find(exercice => exercice.name === fileToOpen)
+          exercice = files
+            .find((element) => element.name === basePath)
+            .exercices.find((exercice) => exercice.name === fileToOpen);
         } else {
           writeToTerminal(`Directory not found: ${basePath}`);
           break;
         }
         console.log(exercice);
-        open(exercice.path)
+        open(exercice.path);
         break;
       default:
         writeToTerminal(`Command not found: ${command}`);
