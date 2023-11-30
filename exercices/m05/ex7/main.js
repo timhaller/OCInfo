5b06e79129fc65c22aab7bda3b8160af9a0ee5ee
timhaller <timhaller@users.noreply.github.com>
2023-11-30 15:50:20 +0000

    Prettified Code!

--- exercices/m05/ex7/main.js
+++ exercices/m05/ex7/main.js
@@ -1,6 +1,6 @@
 const n = askForNumber("Enter a number: ", "N");
 
-let table = ""
+let table = "";
 for (let y = 1; y <= n; y++) {
   let row = "";
   for (let x = 1; x <= n; x++) {
--- exercices/m05/ex9/main.js
+++ exercices/m05/ex9/main.js
@@ -1,17 +1,17 @@
 const essais = askForNumber("Entrez le nombre d'essais: ", "N");
 
-const maxNumber = essais * 5
+const maxNumber = essais * 5;
 const secretNumber = lancerDe(maxNumber);
 let message = "Trouvez le nombre secret entre 1 et " + maxNumber + "\n";
 
 for (let i = 1; i <= essais; i++) {
-    const guess = askForNumber(message + "Essai " + i + ": ", "Z");
-    if (guess === secretNumber) {
-        alert("Vous avez gagné!");
-        break;
-    } else if (guess < secretNumber) {
-        message = "Trop petit!\n";
-    } else {
-        message = "Trop grand!\n";
-    }
+  const guess = askForNumber(message + "Essai " + i + ": ", "Z");
+  if (guess === secretNumber) {
+    alert("Vous avez gagné!");
+    break;
+  } else if (guess < secretNumber) {
+    message = "Trop petit!\n";
+  } else {
+    message = "Trop grand!\n";
+  }
 }
