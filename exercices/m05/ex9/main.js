1b4f8d3be6f0b7d7bc207580263b295961a9122b
timhaller <timhaller@users.noreply.github.com>
2023-11-30 16:46:55 +0000

    Prettified Code!

--- exercices/m05/ex9/main.js
+++ exercices/m05/ex9/main.js
@@ -1,25 +1,28 @@
-const essais = Math.min(askForNumber("Entrez le nombre d'essais: (1 à 3)", "N"), 3);
+const essais = Math.min(
+  askForNumber("Entrez le nombre d'essais: (1 à 3)", "N"),
+  3,
+);
 
-const maxNumber = essais * 5
+const maxNumber = essais * 5;
 const secretNumber = lancerDe(maxNumber);
 let message = "Trouvez le nombre secret entre 1 et " + maxNumber + "\n";
 
 function guess() {
-    for (let i = 1; i <= essais; i++) {
-        const guess = askForNumber(message + "Essai " + i + ": ", "Z");
-        if (guess === secretNumber) {
-            return true;
-        } else if (guess < secretNumber) {
-            message = "Trop petit!\n";
-        } else {
-            message = "Trop grand!\n";
-        }
+  for (let i = 1; i <= essais; i++) {
+    const guess = askForNumber(message + "Essai " + i + ": ", "Z");
+    if (guess === secretNumber) {
+      return true;
+    } else if (guess < secretNumber) {
+      message = "Trop petit!\n";
+    } else {
+      message = "Trop grand!\n";
     }
-    return false;
+  }
+  return false;
 }
 
 if (guess()) {
-    alert("Bravo!");
+  alert("Bravo!");
 } else {
-    alert("Perdu! Le nombre était " + secretNumber);
+  alert("Perdu! Le nombre était " + secretNumber);
 }
--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@
 
 //alert
 function customAlert(text = "") {
-    terminalOutput.innerHTML += `<p>${text}</p>`;
+  terminalOutput.innerHTML += `<p>${text}</p>`;
 }
 
 //since window.alert is a function, we can override it
@@ -10,43 +10,43 @@ window.alert = customAlert;
 
 //prompt
 function prompt(text = "") {
-    return new Promise((resolve) => {
-        terminalOutput.innerHTML += `<span id="promptLine"><span>${text} :</span> <input type="text" id="prompt" /></span>`;
-        const promptInput = document.getElementById("prompt");
-        promptInput.focus();
-        let command = "";
-        promptInput.addEventListener("keydown", function (event) {
-            if (event.key === "Enter") {
-                command = promptInput.value.trim();
-                writeToTerminal(`<span>${text} :</span> ${command}`);
-                promptInput.value = "";
-                const promptLine = document.getElementById("promptLine");
-                promptLine.remove();
-                inputField.focus();
-                resolve(command);
-            }
-        });
+  return new Promise((resolve) => {
+    terminalOutput.innerHTML += `<span id="promptLine"><span>${text} :</span> <input type="text" id="prompt" /></span>`;
+    const promptInput = document.getElementById("prompt");
+    promptInput.focus();
+    let command = "";
+    promptInput.addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        command = promptInput.value.trim();
+        writeToTerminal(`<span>${text} :</span> ${command}`);
+        promptInput.value = "";
+        const promptLine = document.getElementById("promptLine");
+        promptLine.remove();
+        inputField.focus();
+        resolve(command);
+      }
     });
+  });
 }
 
 //confirm
 async function customConfirm(text = "") {
-    return new Promise((resolve) => {
-        terminalOutput.innerHTML += `<span id="confirmLine"><span>${text} [Y/n]</span> <input type="text" id="confirm" /></span>`;
-        const confirmInput = document.getElementById("confirm");
-        confirmInput.focus();
-        let command = "";
-        confirmInput.addEventListener("keydown", function (event) {
-            if (event.key === "Enter") {
-                command = confirmInput.value.trim().toLowerCase();
-                const accepted = command === "y" || command === "";
-                writeToTerminal(`<span>${text} :</span> ${command}`);
-                confirmInput.value = "";
-                const confirmLine = document.getElementById("confirmLine");
-                confirmLine.remove();
-                inputField.focus();
-                resolve(accepted);
-            }
-        });
+  return new Promise((resolve) => {
+    terminalOutput.innerHTML += `<span id="confirmLine"><span>${text} [Y/n]</span> <input type="text" id="confirm" /></span>`;
+    const confirmInput = document.getElementById("confirm");
+    confirmInput.focus();
+    let command = "";
+    confirmInput.addEventListener("keydown", function (event) {
+      if (event.key === "Enter") {
+        command = confirmInput.value.trim().toLowerCase();
+        const accepted = command === "y" || command === "";
+        writeToTerminal(`<span>${text} :</span> ${command}`);
+        confirmInput.value = "";
+        const confirmLine = document.getElementById("confirmLine");
+        confirmLine.remove();
+        inputField.focus();
+        resolve(accepted);
+      }
     });
+  });
 }
